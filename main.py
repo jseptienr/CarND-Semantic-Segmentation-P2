d015fcd0883f1bd8a754b2e6292bd55511a56f8b
Jorge Septien <septien.jorge@gmail.com>
2018-04-13 14:41:45 -0700

    first test

--- main.py
+++ main.py
@@ -120,7 +120,8 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-
+    sess.run(tf.global_variables_initializer())
+    print("Starting training...")
     for epoch in range(epochs):
         for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image: image, correct_label: label, keep_prob: 0.5, learning_rate: 0.0008})
